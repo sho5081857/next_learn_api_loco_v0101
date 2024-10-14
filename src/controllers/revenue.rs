912e26bd6e4f924956b2c55ce23d71507d37fb41
sho5081857 <masashi713416@gmail.com>
2024-10-14 17:58:48 +0900

    Add revenue controller and routes

---
+++ src/controllers/revenue.rs
@@ -0,0 +1,18 @@
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::unnecessary_struct_initialization)]
+#![allow(clippy::unused_async)]
+use loco_rs::prelude::*;
+use axum::debug_handler;
+
+use crate::models::_entities::revenues::Entity;
+
+#[debug_handler]
+pub async fn get_all(_auth: auth::JWT, State(ctx): State<AppContext>) -> Result<Response> {
+    format::json(Entity::find().all(&ctx.db).await?)
+}
+
+pub fn routes() -> Routes {
+    Routes::new()
+        .prefix("revenues")
+        .add("", get(get_all))
+}
