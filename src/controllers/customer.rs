98979fab938a7e8089b18be0e53aca336dcb51e2
sho5081857 <masashi713416@gmail.com>
2024-10-14 19:25:49 +0900

    Add customer controller and routes

---
+++ src/controllers/customer.rs
@@ -0,0 +1,16 @@
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::unnecessary_struct_initialization)]
+#![allow(clippy::unused_async)]
+use loco_rs::prelude::*;
+use axum::debug_handler;
+
+#[debug_handler]
+pub async fn index(State(_ctx): State<AppContext>) -> Result<Response> {
+    format::empty()
+}
+
+pub fn routes() -> Routes {
+    Routes::new()
+        .prefix("customers/")
+        .add("/", get(index))
+}
