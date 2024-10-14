57f0ffa4a9210c123e88ac3a19fd6870b4d9800b
sho5081857 <masashi713416@gmail.com>
2024-10-14 12:04:57 +0900

    first commit

---
+++ src/bin/main.rs
@@ -0,0 +1,8 @@
+use loco_rs::cli;
+use next_learn_api_loco_v0101::app::App;
+use migration::Migrator;
+
+#[tokio::main]
+async fn main() -> loco_rs::Result<()> {
+    cli::main::<App, Migrator>().await
+}
