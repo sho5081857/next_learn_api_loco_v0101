24f3cee1d365dedf282f4c623d46e996446edcb0
sho5081857 <masashi713416@gmail.com>
2024-10-19 23:51:47 +0900

    Add relation between customers and invoices entities

--- src/models/_entities/invoices.rs
+++ src/models/_entities/invoices.rs
@@ -15,4 +15,17 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::customers::Entity",
+        from = "Column::CustomerId",
+        to = "super::customers::Column::Id"
+    )]
+    Customer,
+}
+
+impl Related<super::customers::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Customer.def()
+    }
+}
